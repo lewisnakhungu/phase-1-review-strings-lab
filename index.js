b9a5baa003ad5564bb4604a33018b4a008b5ee01
Lewis <lokiyule@gmail.com>
2025-03-04 19:12:52 +0300

    complete test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Grace Hopper' + 'Welcome to Flatbook, ' + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`; 
